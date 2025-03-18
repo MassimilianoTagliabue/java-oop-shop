6ce06fbd5133a117dd9ce733c8a29e1d17abd98f
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-03-18 22:34:22 +0100

    feat: snack 1

--- src/org/lessons/java/shop/main.java
+++ src/org/lessons/java/shop/main.java
@@ -7,7 +7,7 @@ public class main {
     public static void main(String[] args) {
         Product acqua = new Product("acqua", "bevanda per idratarsi" , new BigDecimal(5), new BigDecimal(0.22));
         
-        
+    
         System.out.println("nome del prodotto = " + acqua.getName() + ", descrizione = " + acqua.getDescription());
         System.out.println("codice del prodotto = " + acqua.getCode() + ", nome completo = " + acqua.getFullName());
         System.out.println("prezzo = " + acqua.getPriceIva());
---
+++ src/snack1/main.java
@@ -0,0 +1,11 @@
+package snack1;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        Studente gino = new Studente("Gino", "pino", 34);
+
+        System.out.println(gino.getDati());
+    }
+}

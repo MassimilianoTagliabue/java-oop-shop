21712c6f45d241d0cbeb1a000e3ca26bf3bea722
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-03-18 19:52:35 +0100

    feat: livelli di accesso

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -7,11 +7,21 @@ import java.util.Random;
 public class Product {
     Random rand = new Random();
 
-    public int code;
-    public String name;
-    public String description;
-    public BigDecimal price;        //i BigDecimal vengono usati quando lavoriamo con le valute
-    public BigDecimal iva;
+    public static String nomeNegozio = "eurospin";
+
+    private int code;
+    private String name;
+    private String description;
+    private BigDecimal price;        //i BigDecimal vengono usati quando lavoriamo con le valute
+    private BigDecimal iva;
+
+    public Product(){
+        this.code = rand.nextInt(1000) + 1; // numero casuale da 1 a 1000
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
 
     public Product(String name, String description, BigDecimal price, BigDecimal iva) {
         this.code = rand.nextInt(1000) + 1; // numero casuale da 1 a 1000
@@ -21,9 +31,42 @@ public class Product {
         this.iva = iva;
     }
 
-    public BigDecimal priceDefault() {
-        return this.price;
+    public int getCode(){
+        return code;
     }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setPrice(BigDecimal price){
+        this.price = price;
+    }
+
+    public BigDecimal getPrice(){
+        return price;
+    }
+
+    public void setIva(BigDecimal iva){
+        this.iva = iva;
+    }
+
+    public BigDecimal getIva(){
+        return iva;
+    }
+
 
     public BigDecimal getPriceIva() {
         if(price != null && iva != null){
--- src/org/lessons/java/shop/main.java
+++ src/org/lessons/java/shop/main.java
@@ -5,11 +5,13 @@ import java.math.BigDecimal;
 public class main {
 
     public static void main(String[] args) {
-        Product prod1 = new Product("acqua", "bevanda per idratarsi" , new BigDecimal(5), new BigDecimal(0.22));
+        Product acqua = new Product("acqua", "bevanda per idratarsi" , new BigDecimal(5), new BigDecimal(0.22));
         
         
-        System.out.println("nome del prodotto = " + prod1.name + ", descrizione = " + prod1.description);
-        System.out.println("codice del prodotto = " + prod1.code + ", nome completo = " + prod1.getFullName());
-        System.out.println("prezzo = " + prod1.getPriceIva());
+        System.out.println("nome del prodotto = " + acqua.getName() + ", descrizione = " + acqua.getDescription());
+        System.out.println("codice del prodotto = " + acqua.getCode() + ", nome completo = " + acqua.getFullName());
+        System.out.println("prezzo = " + acqua.getPriceIva());
+
+        
     }
 }
